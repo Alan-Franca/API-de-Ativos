a519c4f13c769a81d1a8739161e21f9585a567dc
Alan-Franca <alanfrancati@gmail.com>
2025-10-27 22:23:24 -0300

    refazendo o infra e adicionando uma nova feacture

--- app/index.tsx
+++ app/index.tsx
@@ -2,24 +2,15 @@ import React, { useState } from "react";
 import {
   View,
   Text,
-  TextInput,
-  StyleSheet,
   FlatList,
   ActivityIndicator,
   Alert,
   TouchableOpacity,
-  Platform,
-  Image,
 } from "react-native";
 import axios from "axios";
-
-type Asset = {
-  ticker: string;
-  price: number;
-  dividendYield: number;
-  type: "Ação" | "FII";
-  logo: string;
-};
+import { styles, colors } from "./styles";
+import { AssetCard, Asset } from "../components/AssetCard/AssetCard";
+import { PriceSlider } from "../components/PriceSlider/PriceSlider";
 
 const BRAPI_API_TOKEN = process.env.EXPO_PUBLIC_BRAPI_API_TOKEN;
 const BRAPI_BASE_URL = "https://brapi.dev/api";
@@ -69,15 +60,18 @@ const fetchAssetDetails = async (ticker: string): Promise<Asset | null> => {
 };
 
 export default function CalculadoraScreen() {
-  const [investValue, setInvestValue] = useState<string>("");
+  const [priceRange, setPriceRange] = useState<[number, number]>([10, 30]);
   const [finalAssets, setFinalAssets] = useState<Asset[]>([]);
   const [wasCalculated, setWasCalculated] = useState<boolean>(false);
   const [isLoading, setIsLoading] = useState<boolean>(false);
 
   const handleFilterAssets = async () => {
-    const maxPrice = parseFloat(investValue);
-    if (isNaN(maxPrice) || maxPrice <= 0) {
-      Alert.alert("Valor Inválido", "Por favor, insere um número positivo.");
+    const [minPrice, maxPrice] = priceRange;
+    if (minPrice > maxPrice) {
+      Alert.alert(
+        "Intervalo Inválido",
+        "O preço mínimo não pode ser maior que o preço máximo."
+      );
       return;
     }
 
@@ -88,7 +82,7 @@ export default function CalculadoraScreen() {
     try {
       const basicList = await fetchAssetsList();
       const priceFilteredList = basicList.filter(
-        (asset) => asset.price <= maxPrice
+        (asset) => asset.price >= minPrice && asset.price <= maxPrice
       );
       priceFilteredList.sort((a, b) => a.price - b.price);
 
@@ -102,7 +96,7 @@ export default function CalculadoraScreen() {
       );
 
       detailedAssets.sort((a, b) => b.dividendYield - a.dividendYield);
-      setFinalAssets(detailedAssets.slice(0, 100));
+      setFinalAssets(detailedAssets.slice(0, 30));
     } catch (error) {
       if (error instanceof Error) Alert.alert("Erro", error.message);
     } finally {
@@ -133,31 +127,14 @@ export default function CalculadoraScreen() {
     return (
       <View style={styles.resultsContainer}>
         <Text style={styles.resultsTitle}>
-          Melhores Ativos até R$ {investValue}
+          Top 30 Ativos entre R$ {priceRange[0].toFixed(2)} e R${" "}
+          {priceRange[1].toFixed(2)}
         </Text>
         <FlatList
-          style={{ flex: 1 }}
           data={finalAssets}
           keyExtractor={(item) => item.ticker}
-          renderItem={({ item }) => (
-            <View style={styles.card}>
-              <View style={{ flexDirection: "row", alignItems: "center" }}>
-                {item.logo ? (
-                  <Image source={{ uri: item.logo }} style={styles.logo} />
-                ) : (
-                  <View style={styles.logoPlaceholder} />
-                )}
-                <View style={{ flex: 1 }}>
-                  <Text style={styles.ticker}>
-                    {item.ticker} ({item.type})
-                  </Text>
-                  <Text style={styles.totalItem}>
-                    Valor da Cota: R$ {item.price.toFixed(2)}
-                  </Text>
-                </View>
-              </View>
-            </View>
-          )}
+          renderItem={({ item }) => <AssetCard item={item} />}
+          style={{ flex: 1 }}
         />
       </View>
     );
@@ -166,14 +143,21 @@ export default function CalculadoraScreen() {
   return (
     <View style={styles.container}>
       <Text style={styles.header}>Filtro de Ativos</Text>
-      <TextInput
-        style={styles.input}
-        placeholder="Preço máximo por cota (ex: 300)"
-        placeholderTextColor={colors.darkBeige}
-        keyboardType="numeric"
-        value={investValue}
-        onChangeText={setInvestValue}
+
+      <PriceSlider
+        label="Preço Mínimo"
+        value={priceRange[0]}
+        onValueChange={(value) => setPriceRange([value, priceRange[1]])}
+        maximumValue={200}
       />
+
+      <PriceSlider
+        label="Preço Máximo"
+        value={priceRange[1]}
+        onValueChange={(value) => setPriceRange([priceRange[0], value])}
+        maximumValue={500}
+      />
+
       <TouchableOpacity
         style={styles.button}
         onPress={handleFilterAssets}
@@ -187,99 +171,3 @@ export default function CalculadoraScreen() {
     </View>
   );
 }
-
-const colors = {
-  darkBlue: "#243757",
-  tealBlue: "#3A5F6F",
-  lightBeige: "#DAD5B7",
-  darkBeige: "#C2B79B",
-  brownGray: "#665E52",
-  white: "#FFFFFF",
-};
-
-const styles = StyleSheet.create({
-  container: { flex: 1, padding: 16, backgroundColor: colors.lightBeige },
-  header: {
-    fontSize: 24,
-    fontWeight: "bold",
-    textAlign: "center",
-    marginBottom: 20,
-    paddingTop: 20,
-    color: colors.darkBlue,
-  },
-  input: {
-    height: 50,
-    borderColor: colors.darkBeige,
-    borderWidth: 1,
-    borderRadius: 8,
-    paddingHorizontal: 10,
-    marginBottom: 20,
-    fontSize: 18,
-    backgroundColor: colors.white,
-    color: colors.brownGray,
-  },
-  button: {
-    backgroundColor: colors.darkBlue,
-    padding: 15,
-    borderRadius: 8,
-    alignItems: "center",
-  },
-  buttonText: { color: colors.white, fontSize: 16, fontWeight: "bold" },
-  resultsContainer: { flex: 1, marginTop: 20 },
-  resultsTitle: {
-    fontSize: 20,
-    fontWeight: "bold",
-    marginBottom: 10,
-    color: colors.darkBlue,
-  },
-  card: {
-    backgroundColor: colors.white,
-    padding: 16,
-    borderRadius: 8,
-    marginBottom: 12,
-    borderColor: colors.darkBeige,
-    borderWidth: 1,
-    ...Platform.select({
-      ios: {
-        shadowColor: "#000",
-        shadowOffset: { width: 0, height: 2 },
-        shadowOpacity: 0.23,
-        shadowRadius: 2.62,
-      },
-      android: { elevation: 4 },
-      web: { boxShadow: "0 2px 4px rgba(0,0,0,0.15)" },
-    }),
-  },
-  logo: {
-    width: 40,
-    height: 40,
-    borderRadius: 20,
-    marginRight: 12,
-    backgroundColor: "#eee",
-  },
-  logoPlaceholder: {
-    width: 40,
-    height: 40,
-    marginRight: 12,
-  },
-  ticker: { fontSize: 18, fontWeight: "bold", color: colors.tealBlue },
-  totalItem: {
-    fontWeight: "bold",
-    color: colors.tealBlue,
-    fontSize: 14,
-    marginTop: 2,
-  },
-  dyText: {
-    color: colors.brownGray,
-    marginTop: 8,
-    textAlign: "right",
-    fontSize: 14,
-    fontWeight: "500",
-  },
-  noResultsText: {
-    textAlign: "center",
-    marginTop: 40,
-    fontSize: 16,
-    color: colors.brownGray,
-  },
-});
